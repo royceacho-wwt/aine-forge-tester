9db7c363efe76f2a977995e642648077a0ed532f
Forge <forge@users.noreply.github.com>
2026-07-28 17:03:44 +0000

    Add descriptive comment at top of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// Main App component - root of the React application
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
